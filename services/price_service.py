85580a1b9be15ee35a9f69d50707c9e768500ab8
PreeabChivin <Preabchivin007@gmail.com>
2026-04-22 09:51:20 +0700

    fix api key

--- services/price_service.py
+++ services/price_service.py
@@ -1,22 +1,19 @@
+import os
 import requests
 import time
 
 def get_prices(retries=3, delay=2):
-    headers = {"x-access-token": "goldapi-74cd78b1bdb541a5b2ba9ecb0b26a939-io"}
-
+    headers = {"x-access-token": os.getenv("GOLDAPI_KEY")}
     for attempt in range(retries):
         try:
             gold_res = requests.get("https://www.goldapi.io/api/XAU/USD", headers=headers, timeout=10)
             silver_res = requests.get("https://www.goldapi.io/api/XAG/USD", headers=headers, timeout=10)
 
             print("Gold status:", gold_res.status_code)
-            print("Gold raw:", gold_res.text)
-            print("Silver status:", silver_res.status_code)
-            print("Silver raw:", silver_res.text)
+            print("Gold raw:", gold_res.text)  # <-- add this
 
             gold_price = gold_res.json()["price"]
             silver_price = silver_res.json()["price"]
-
             return {"gold": gold_price, "silver": silver_price}
 
         except Exception as e:
